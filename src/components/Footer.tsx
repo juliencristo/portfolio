384d287ee343948181cac9686f1cff1525287011
Julien Cristovao <julien.cristovao@gmail.com>
2026-04-17 17:22:19 +0200

    delete bg footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-[hsl(var(--card))] text-[hsl(var(--primary))] py-6 mt-12 border-t-[hsl(var(--border))]">
+        <footer className="text-[hsl(var(--primary))] py-6 mt-12 border-t-[hsl(var(--border))]">
             <div className="max-w-7xl mx-auto px-4 text-center">
                 <p className="text-sm">&copy; {new Date().getFullYear()} Julien Cristovao . All rights reserved.</p>
                 <p className="text-sm mt-2">Designed and built by me with React and Tailwind CSS.</p>
